2439b593f8b40af902904cd998a3080438b0a2ce
nzying <57226752+nzying@users.noreply.github.com>
2020-11-17 23:10:17 +0800

    latest

--- src/AdminManager.java
+++ src/AdminManager.java
@@ -14,7 +14,12 @@ public class AdminManager {
 		String courseCode = sc.next().toUpperCase();
 		for(int i=0;i<app.getCourse().size();i++) {
 			if(courseCode.equals(app.getCourse().get(i).getCourseCode())) {
-				app.getCourse().get(i).printVacancy();
+                for (int j=0; j<app.getCourse().get(i).getIndex().size(); j++)
+                {
+                    int sum = 0;
+                    sum += app.getCourse().get(i).getIndex().get(j).getVacancy();
+                }
+				
 			}
 		}
     }
@@ -26,19 +31,15 @@ public class AdminManager {
         app.printCourse();
         System.out.println("Enter the course code of the course that you wish to print the student list from:");
         String courseCode = sc.next().toUpperCase();
-        for (int i=0; i<app.getCourse().size(); i++){
+        for (int i=0; i<app.getStudent().size(); i++){
             Student s = app.getStudent().get(i);
             Course c = app.getCourse().get(i);
             if (courseCode.equals(c.getCourseCode()) && courseCode.equals(s.getCourseRegistered().get(i).getCourseCode())){
                 app.getCourse().get(i).printVacancy();
                 System.out.println("Enter the course index that you wish to print the student list from:");
                 String courseIndex = sc.next();
-                for (int j=0; j<s.getCourseRegistered().size(); j++){
-                    if (courseIndex.equals(s.getCourseRegistered().get(j).getIndexNo())){
-                        System.out.println(s.getUserName() + "\t" + s.getName());
-                    }
-                }
-                
+                if (courseIndex.equals(s.getCourseRegistered().get(i).getIndexNo()))
+                    System.out.println(s.getUserName() + "\t" + s.getName());
                
             }
             
@@ -51,7 +52,7 @@ public class AdminManager {
         app.printCourse();
         System.out.println("Enter the course code of the course that you wish to print the student list from:");
         String courseCode = sc.next().toUpperCase();
-        for (int i=0; i<app.getCourse().size(); i++)
+        for (int i=0; i<app.getStudent().size(); i++)
         {
             Student s = app.getStudent().get(i);
             Course c = app.getCourse().get(i);
@@ -59,8 +60,6 @@ public class AdminManager {
                 System.out.println(s.getUserName() + "\t" + s.getName());
                 
             }
-            System.out.println();
-            break;
         }
         
 
