6a78116b279aa648573335fde9f13ed2a240ee5b
therealnicklaus <nicklauswei@yahoo.com.sg>
2020-11-25 02:45:36 +0800

    Enter

--- src/StarsApp.java
+++ src/StarsApp.java
@@ -31,6 +31,7 @@ public class StarsApp {
 			int choice = 0;
 			do{
 				System.out.println();
+				System.out.println("Admin Options:");
 				System.out.println("1. Edit student access period");
 				System.out.println("2. Add a student");
 				System.out.println("3. Delete a student");
@@ -75,6 +76,7 @@ public class StarsApp {
 			
 			do {
 			System.out.println();
+			System.out.println("Student Options:");
 			System.out.println("1. Register a course");
 			System.out.println("2. Drop a course");
 			System.out.println("3. Check/Print Courses Registered");
